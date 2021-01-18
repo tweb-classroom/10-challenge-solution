3d9bbe97f49027acb894d9e59ad9a891cb7f54c5
paulnta <paul.ntawuruhunga@gmail.com>
2021-01-18 17:44:58 +0100

    feat: add solution

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import logger from "morgan";
+import logos from "./logos.json";
+import { randomArray, compare } from './utils/index.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,23 +9,30 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
 app.use(express.static("public"));
 
 // TODO question 1: Retourner 8 logo choisis aléatoirement
 app.get("/", (_, res) => {
-  res.render("game", {
-    logos: [
-      { id: "react" },
-      { id: "svelte-icon" },
-      { id: "vue" },
-      { id: "angular-icon" },
-    ],
-  });
+  res.render("game", { logos: randomArray(logos, 8) });
 });
 
 // TODO: question 3: ajouter un endpoint pour valider les réponses du joueur
 // puis retourner le nombre de réponses corrects et les solutions
+app.post("/validate", (req, res) => {
+  const items = Object.entries(req.body).map(([id, answer]) => {
+    const logo = logos.find((logo) => logo.id === id);
+    return {
+      id: logo.id,
+      name: logo.name,
+      valid: compare(answer, logo.name),
+    };
+  });
+
+  res.json({
+    logos: items,
+    score: items.filter((item) => item.valid).length,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Magic happens at http://localhost:${port} 🚀`);
--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
 import chai from "chai";
+import { compare } from "../utils/index.js";
 
 const { assert } = chai;
 
 // TODO: Ajouter un test pour valider la logique de validation des réponses
 // La casse et les charactères spéciaux doivent être ignorés
+describe('compare', () => {
+  it("should ignore case and special chars", () => {
+    assert.equal(compare('NextJS', 'next.js'), true);
+  });
+})
 
-it("should work", () => {
-  assert.equal(true, true);
-});
